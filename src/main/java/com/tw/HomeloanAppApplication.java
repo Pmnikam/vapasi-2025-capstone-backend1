d608c75d6024be5e45bb14b91a301cc5b716bb45
pratibhanikamkatkar <pratibhanikam98@yahoo.com>
2025-07-21 17:35:08 +0530

    VW_DB_100 Pratibha|Prabhjot add project structure

---
+++ src/main/java/com/tw/HomeloanAppApplication.java
@@ -0,0 +1,13 @@
+package com.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeloanAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeloanAppApplication.class, args);
+    }
+
+}
