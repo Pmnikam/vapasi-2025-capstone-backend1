0358ff24b5613a166e7e4362985dab21a18b9631
writemedivi <117740915+writemedivi@users.noreply.github.com>
2025-07-22 15:38:06 +0530

    VW-105 Dhivya : Modified spring security for login and signup with cross origin

--- src/main/java/com/tw/controller/UserController.java
+++ src/main/java/com/tw/controller/UserController.java
@@ -6,11 +6,9 @@ import com.tw.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,7 +21,7 @@ public class UserController {
         return ResponseEntity.ok("Signup successful");
     }
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody LoginDto loginDto) {
         userService.login(loginDto);
         return ResponseEntity.ok("Login successful");
