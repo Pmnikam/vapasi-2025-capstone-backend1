52d98f28a6b7a1a0e1007ebff6724e283979ddad
anuradhapai <anuradhashenoy4@gmail.com>
2025-07-23 21:17:31 +0530

    VWB-023 Anuradha: Removed unused exception classes

--- src/main/java/com/tw/exception/FailedToReadDocument.java
+++
@@ -1,7 +0,0 @@
-package com.tw.exception;
-
-public class FailedToReadDocument extends RuntimeException {
-    public FailedToReadDocument(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/tw/exception/InvalidLoanApplicationException.java
+++
@@ -1,7 +0,0 @@
-package com.tw.exception;
-
-public class InvalidLoanApplicationException extends RuntimeException {
-    public InvalidLoanApplicationException(String message) {
-        super(message);
-    }
-}
