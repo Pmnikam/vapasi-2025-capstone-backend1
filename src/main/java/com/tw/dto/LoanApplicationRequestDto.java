e995ec090c683dd146763a3e20f0e658f6301ad3
writemedivi <117740915+writemedivi@users.noreply.github.com>
2025-07-24 09:28:50 +0530

    VWB-015 Anuradha|Dhivya: Merge conflict issue for implement Admin Service to retrieve the list of customer loan applications and enable approval/rejection workflow based on loan status.

--- src/main/java/com/tw/dto/LoanApplicationRequestDto.java
+++ src/main/java/com/tw/dto/LoanApplicationRequestDto.java
@@ -4,13 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LoanApplicationRequestDto {
 
     @NotBlank(message = "Date of birth is required")
